a5f2a168bc8a1b5fea023e2552fe76c675a108e0
AaronRojas-579 <aaron.rojaas@outlook.com>
2022-11-11 12:40:53 -0300

    Desafio 23

---
+++ notas.md
@@ -0,0 +1,18 @@
+## Cookies
+Es semenjante al localStorage del js
+### Explicacion de los archivos 
+creados 
+<br>
+* Public
+    * index.html
+    * index.js
+
+### Sirve para el uso del Middleware app.use(express.static("public"))
+<br>
+
+* middleware
+
+### Guadarmos todos los Middleware para luego exportalos y usarlos en app.js
+
+* .env
+### Archivo que utilizaremos para instanciar las variables de entorno
---
+++ server.js
@@ -0,0 +1,12 @@
+//Import con commonjs app 
+const app = require("./app.js")
+// Este dotenv sirve para utilizar las variales de entorno
+//Requiere de instalación (npm install dotenv)
+require("dotenv").config()
+//process.(nombre del archivo).(nombre de la variable de entorno)
+const PORT = process.env.PORT || 3000
+// en el caso que no exista PORT será a 3000
+
+app.listen(PORT,()=>{
+    console.log(`Server listen on PORT ${PORT}`)
+})
